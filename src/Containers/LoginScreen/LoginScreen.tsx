375f0843715d653b9b8d29ee06cbac073b26344a
Mareaucanard <matthieu.fraiz@epitech.eu>
2022-05-23 14:13:44 +0200

    Add start of login page
    The form doesn't work and the looks aren't right either

--- src/Containers/LoginScreen/LoginScreen.tsx
+++ src/Containers/LoginScreen/LoginScreen.tsx
@@ -1,15 +1,42 @@
-import React, { useEffect } from 'react'
-import { View, Text } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { View, Text, Image, TextInput} from 'react-native'
 import { Common } from '@/Theme'
+import { BasicButton } from '@/Components'
 
+import HandleLoginSumbit from './HandleLoginSumbit'
 import styles from './LoginScreenStyles'
+const image = require('@/Assets/Images/Illustrations/login-background.png')
+
+
+const LoginForm = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleSubmit = () => {
+        HandleLoginSumbit(email, password)
+    }
+    return (
+        <View style={[styles.contentContainer]}>
+            <Text style={styles.title}>Connexion</Text>
+
+            <Text style={styles.sectionTitle}>Adress email</Text>
+            <TextInput style={[Common.basicShadow, Common.textInput, styles.input]} value={email} onChangeText={setEmail}/>
+
+            <Text style={styles.sectionTitle}>Mot de passe</Text>
+            <TextInput style={[Common.basicShadow, Common.textInput, styles.input]} value={password} onChangeText={setPassword}/>
+
+            <BasicButton text={"Se connecter"} onPress={handleSubmit} style={[Common.backgroundPrimary]}/>
+        </View>
+    )
+}
 
 const LoginScreen = () => {
     useEffect(() => {}, [])
 
     return (
         <View style={[Common.basicPage]}>
-            <Text>Good luck</Text>
+            <Image source={image} style={styles.backgroundImage}/>
+            <LoginForm />
         </View>
     )
 }
