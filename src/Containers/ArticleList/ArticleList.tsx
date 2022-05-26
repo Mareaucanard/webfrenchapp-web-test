409bb3fb59ac08e7ddde81e46b565f680cd193ac
Mareaucanard <matthieu.fraiz@epitech.eu>
2022-05-26 20:56:29 +0200

    Add ugly but working prototype

--- src/Containers/ArticleList/ArticleList.tsx
+++ src/Containers/ArticleList/ArticleList.tsx
@@ -1,23 +1,28 @@
 import React, { useState, useEffect } from "react"
-import { View, Text, Button } from "react-native";
+import { View, Text, ImageBackground } from "react-native";
 
 import { Common } from "@/Theme";
 import styles from "./ArticleListStyles"
 import GetNewsList from "./FetchNews";
-import BasicButtonStyles from "@/Components/BasicButton/BasicButtonStyles";
-import { BasicButton } from "@/Components";
+import { ScrollView } from "react-native-gesture-handler";
 
 function Article(props: any) {
     const { news_data } = props
+    const date = new Date ( news_data.date )
+    var date_string = date.getDay()
+
     return (
-        <View style={styles.content}>
+        <View style={styles.content}
+        onStartShouldSetResponder={() => console.log("Clicked data" + news_data.title)}>
+            <ImageBackground resizeMode="cover" source={{uri: news_data.image}} style={styles.content}>
             <Text style={styles.sectionTitle}>{news_data.title}</Text>
-            <Text>{news_data.body}</Text>
+            <Text>{date_string}</Text>
+            </ImageBackground>
         </View>
     )
 }
 
-function ArticleList(props: Object) {
+function ArticleList(props: any) {
     var [news, updateNews] = useState(undefined)
     useEffect(() => GetNewsList(props.token, updateNews), []) // This is here to only GetNewsList once, else it would infinite loop
 
@@ -26,11 +31,11 @@ function ArticleList(props: Object) {
     } else if (news === false) {
         return <Text>Erreur</Text>
     } else {
-        return news.map((news_data: Object) => <Article news_data={news_data} key={news_data.id} />)
+        return news.map((news_data: any) => <Article news_data={news_data} key={news_data.id} />)
     }
 }
 
-function ArticleListPage(parameters: Object) {
+function ArticleListPage(parameters: any) {
     const { route } = parameters
     const { params } = route
     const { token , refreshToken } = params
@@ -38,7 +43,9 @@ function ArticleListPage(parameters: Object) {
     return (
         <View style={Common.basicPage}>
             <Text style={styles.title}>Actualité</Text>
-            <ArticleList token={token}/>
+            <ScrollView style={Common.basicPage}>
+                <ArticleList token={token}/>
+            </ScrollView>
         </View>
     )
 }
