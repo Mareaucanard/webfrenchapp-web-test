37eab549e797f8465b91bc0ea300c1e802765c8c
Mareaucanard <matthieu.fraiz@epitech.eu>
2022-05-28 18:25:13 +0200

    Fix GoBackButton and made it prettier

--- src/Components/GoBackButton/GoBackButton.tsx
+++ src/Components/GoBackButton/GoBackButton.tsx
@@ -2,20 +2,23 @@ import React from "react"
 import { Image, View } from "react-native"
 import { StackActions } from '@react-navigation/native';
 import styles from "./GoBackStyle"
+import { navigationRef } from "@/Navigators/Root";
+import { TouchableHighlight } from "react-native-gesture-handler";
 
 const image = require("@/Assets/Images/Icons/Basic/left-arrow.png")
 
 function my_function() {
     console.log("GoBackButton was pressed")
-    StackActions.pop(1)
+    navigationRef.current?.dispatch(StackActions.pop());
     return true
 }
 
 function GoBackButton() {
     return (
-        <View style={styles.goBackButton}
-        onStartShouldSetResponder={() => my_function()}>
-            <Image source={image}></Image>
+        <View style={styles.goBackButton}>
+            <TouchableHighlight onPress={() => my_function()} style={styles.touchable} underlayColor="#00000020">
+                <Image source={image}></Image>
+            </TouchableHighlight>
         </View>
     )
 }
--- src/Components/GoBackButton/GoBackStyle.ts
+++ src/Components/GoBackButton/GoBackStyle.ts
@@ -6,5 +6,13 @@ export default StyleSheet.create({
         position: "absolute",
         paddingTop: Metrics.heightPercentageToDP("4%") + (Metrics.isIphoneX() ? 60 : 40),
         paddingLeft: Metrics.widthPercentageToDP("10%"),
+        zIndex: 1,
+        borderRadius: 1,
+    },
+    touchable: {
+        borderRadius: 15,
+        borderWidth: 1,
+        overflow: "hidden",
+        borderColor: "transparent",
     }
 })
